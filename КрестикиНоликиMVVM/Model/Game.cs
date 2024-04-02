b494a388c2e0143b1a75a80b925c771b7b231d4c
KOGADO <kirill.lashev@gmail.com>
2024-04-02 17:26:01 +0300

    help

---
+++ КрестикиНоликиMVVM/Model/Game.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TicTacToeWPF_MVVM.Model
+{
+    public class Game
+    {
+        public List<Player> Players { get; set; } = new List<Player>();
+        public string CurrentMark { get; set; }
+        private List<string> MarksToAssign { get; set; } = new List<string>();
+        public string Winner { get; set; }
+        public bool IsFinished { get; set; }
+
+        public Game(Player p1, Player p2)
+        {
+            IsFinished = false;
+
+            Players.Add(p1);
+            Players.Add(p2);
+
+            MarksToAssign.Add("X");
+            MarksToAssign.Add("O");
+            AssignMark();
+
+            foreach (var player in Players)
+            {
+                Console.WriteLine($"PlayerInfo: {player.Name}, {player.Mark}, {player.IsTurn}");
+            }
+
+            AssignTurn();
+
+            foreach (var player in Players)
+            {
+                Console.WriteLine($"PlayerInfo after turn assignment: {player.Name}, {player.Mark}, {player.IsTurn}");
+            }
+        }
+
+        public string WhoseRound()
+        {
+            var player = Players.First(x => x.IsTurn == false);
+
+            return $"Ход игрока: {player.Name}, его сторона {player.Mark}";
+        }
+
+        private void ChangeTurn(Player player)
+        {
+            player.IsTurn = !player.IsTurn;      
+        }
+
+        public void ChangeTurns(string[] field)
+        {
+            foreach (var player in Players)
+            {
+                if (CheckWin(field) == true)
+                {
+                    var winner = Players.FirstOrDefault(x => x.IsWinner == true);
+                    MessageBox.Show($"Победитель - {winner.Name}, сторона игрока {winner.Mark}");
+                    IsFinished = true;
+                    break;
+                }
+                else if (IsTie(field))
+                {
+                    MessageBox.Show("Ничья!");
+                    IsFinished = true;
+                    break;
+                }
+
+                if (player.IsTurn)
+                {
+                    CurrentMark = player.Mark;
+                }
+
+                ChangeTurn(player);
+            }
+        }
+
+        private void AssignTurn()
+        {
+            var player = Players.FirstOrDefault(x => x.Mark == "X");
+            player.IsTurn = true;
+        }
+
+        private bool CheckWin(string[] table)
+        {
+            var conditionToWin1 = CheckWinHorizontal(table);
+            var conditionToWin2 = CheckWinVertical(table);
+            var conditionToWin3 = CheckWinDiagonal(table);
+
+            return conditionToWin1 || conditionToWin2 || conditionToWin3;
+        }
+
+        private bool CheckWinDiagonal(string[] table)
+        {
+            if ((table[0] + table[4] + table[8]) == "XXX")
+            {
+                var winner = Players.FirstOrDefault(x => x.Mark == "X");
+                winner.IsWinner = true;
+
+                return true;
+            }
+
+            if ((table[0] + table[4] + table[8]) == "OOO")
+            {
+                var winner = Players.FirstOrDefault(x => x.Mark == "O");
+                winner.IsWinner = true;
+
+                return true;
+            }
+
+            if ((table[2] + table[4] + table[6]) == "XXX")
+            {
+                var winner = Players.FirstOrDefault(x => x.Mark == "X");
+                winner.IsWinner = true;
+
+                return true;
+            }
+
+            if ((table[2] + table[4] + table[6]) == "OOO")
+            {
+                var winner = Players.FirstOrDefault(x => x.Mark == "O");
+                winner.IsWinner = true;
+
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool CheckWinVertical(string[] table)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                string txt = table[i] + table[i + 3] + table[i + 6];
+                if (txt == "XXX")
+                {
+                    var winner = Players.FirstOrDefault(x => x.Mark == "X");
+                    winner.IsWinner = true;
+                    
+                    return true;
+                }
+
+                if (txt == "OOO")
+                {
+                    var winner = Players.FirstOrDefault(x => x.Mark == "O");
+                    winner.IsWinner = true;
+
+                    return true;
+                }
+                    
+            }
+
+            return false;
+        }
+
+        private bool CheckWinHorizontal(string[] table)
+        {
+            for (int i = 0; i < 9; i += 3)
+            {
+                string txt = table[i] + table[i + 1] + table[i + 2];
+                if (txt == "XXX")
+                {
+                    var winner = Players.FirstOrDefault(x => x.Mark == "X");
+                    winner.IsWinner = true;
+
+                    return true;
+                }
+
+                if (txt == "OOO")
+                {
+                    var winner = Players.FirstOrDefault(x => x.Mark == "O");
+                    winner.IsWinner = true;
+
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private bool IsTie(string[] table)
+        {
+            var txt = "";
+
+            foreach(var element in table)
+            {
+                txt += element;
+            }
+
+            if (txt.Length == 9 && !String.IsNullOrWhiteSpace(txt))
+                return true;
+            
+            return false;
+        }
+
+        private void AssignMark()
+        {
+            var random = new Random();
+
+            foreach (var player in Players)
+            {
+                int index = random.Next(MarksToAssign.Count);
+                player.Mark = MarksToAssign.ElementAt(index);
+                MarksToAssign.RemoveAt(index);
+            }
+        }
+    }
+}
---
+++ КрестикиНоликиMVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicTacToeWPF_MVVM.Model;
+
+namespace TicTacToeWPF_MVVM.ViewModel
+{
+    using BaseClass;
+    using System.Windows.Controls;
+    using System.Windows.Input;
+    public class MainViewModel : ViewModel
+    {
+
+     
+
+
+        private Game _ngame;
+
+        private bool _gameInProgress = false;
+
+        public string[] _table = new string[] { "", "", "", "", "", "", "", "", "" };
+
+        public string[] Table
+        {
+            get { return _table; }
+            set
+            {
+                _table = value;
+                onPropertyChange(nameof(Table));
+            }
+        }
+
+        private string _whoseRound;
+
+        public string WhoseRound
+        {
+            get
+            {
+                return _whoseRound;
+            }
+            set
+            {
+                _whoseRound = value;
+                onPropertyChange(nameof(WhoseRound));
+            }
+        }
+
+
+        public MainViewModel()
+        {
+        }
+
+        private string _playerName1;
+
+        public string PlayerName1
+        {
+            get { return _playerName1; }
+            set
+            {
+                _playerName1 = value;
+                onPropertyChange(nameof(PlayerName1));
+            }
+        }
+
+        private string _playerName2;
+
+        public string PlayerName2
+        {
+            get { return _playerName2; }
+            set
+            {
+                _playerName2 = value;
+                onPropertyChange(nameof(PlayerName2));
+            }
+        }
+
+        private ICommand _onClick;
+
+        public ICommand OnClick
+        {
+            get
+            {
+                return _onClick ?? (_onClick = new RelayCommand(OnClickFunc, CanOnClick));
+            }
+        }
+
+        private void OnClickFunc(object obj)
+        {
+            int field = int.Parse((string)obj);
+
+            Table[field] = _ngame.CurrentMark;
+
+            Table = Table;
+
+            _ngame.ChangeTurns(Table);
+            WhoseRound = _ngame.WhoseRound();
+
+            if (_ngame.IsFinished)
+            {
+                Table = new string[] { "", "", "", "", "", "", "", "", "" };
+                PlayerName1 = "";
+                PlayerName2 = "";
+                _gameInProgress = false;
+                WhoseRound = "";
+            }
+        }
+
+        private bool CanOnClick(object param)
+        {
+            int field = int.Parse((string)param);
+
+            if (!_gameInProgress)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < Table.Length; i++)
+            {
+                if (Table[field] != "")
+                    return false;
+            }
+
+            return true;
+        }
+
+        private ICommand _start;
+
+        public ICommand Start
+        {
+            get
+            {
+                return _start ?? (_start = new RelayCommand(StartGame, CanStartGame));
+            }
+        }
+
+        private void StartGame(object param)
+        {
+            var player1 = new Player(_playerName1);
+            var player2 = new Player(_playerName2);
+            _ngame = new Game(player1, player2);
+            _gameInProgress = true;
+
+            _ngame.ChangeTurns(Table);
+
+            WhoseRound = _ngame.WhoseRound();
+        }
+
+        private bool CanStartGame(object param)
+        {
+            var textBoxEmpty = !(String.IsNullOrEmpty(_playerName1) || String.IsNullOrEmpty(_playerName2));
+
+            return (textBoxEmpty && !_gameInProgress);
+        }
+    }
+}
